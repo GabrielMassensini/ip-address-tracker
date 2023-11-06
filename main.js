6831c8bda464bdb46baa07721f192f13718a25de
Gabriel Massensini <137442121+GabrielMassensini@users.noreply.github.com>
2023-11-06 12:28:25 -0300

    map generated and the ip locator is working, you need to be able to make the map change location

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
-let ipLocation = "8.8.8.8"
+const ipLocation = document.getElementById("idInput");
+const searchLocationBtn = document.getElementById("searchId");
 
-const map = () => {
-  var map = L.map("map").setView([32.69922, -117.11281], 13);
-  var marker = L.marker([32.69922, -117.11281]).addTo(map);
+const ipHTML = document.getElementById("ipHTML");
+const locationHTML = document.getElementById("locationHTML");
+const timezoneHTML = document.getElementById("timezoneHTML");
+const ispHTML = document.getElementById("ispHTML");
+
+const IpGeolocation = async (ip = "8.8.8.8") => {
+  const api = await fetch(
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_Gm32QfjbHBkhFsRk7vgdcaJAwSw0X&ipAddress=${ip}`
+  );
+  const data = await api.json();
+  let lat = 0;
+  let lng = 0;
+
+  ipHTML.innerHTML = data.ip;
+  locationHTML.innerHTML = `${data.location.city}, ${data.location.region}`;
+  timezoneHTML.innerHTML = `UTC ${data.location.timezone}`;
+  ispHTML.innerHTML = data.isp;
+  lat = data.location.lat;
+  lng = data.location.lng;
+
+  var map = L.map("map").setView([lat, lng], 1);
+  var marker = L.marker([lat, lng]).addTo(map);
 
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
-
-  const IpGeolocation = async () => {
-    const api = await fetch(
-      `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_Gm32QfjbHBkhFsRk7vgdcaJAwSw0X&ipAddress=${ipLocation}`
-    );
-    const data = await api.json();
-    console.log(data);
-  };
-
-  IpGeolocation();
 };
 
-map();
+IpGeolocation();
+
+searchLocationBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const location = ipLocation.value;
+  IpGeolocation(location);
+});
