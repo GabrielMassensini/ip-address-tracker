b5998c3794881a9131083342a4c22ee508f4fb8e
Gabriel Massensini <137442121+GabrielMassensini@users.noreply.github.com>
2023-11-09 08:41:10 -0300

    app is finished

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const locationHTML = document.getElementById("locationHTML");
 const timezoneHTML = document.getElementById("timezoneHTML");
 const ispHTML = document.getElementById("ispHTML");
 
+var map;
+var marker;
+
 const IpGeolocation = async (ip = "8.8.8.8") => {
   const api = await fetch(
     `https://geo.ipify.org/api/v2/country,city?apiKey=at_Gm32QfjbHBkhFsRk7vgdcaJAwSw0X&ipAddress=${ip}`
@@ -21,8 +24,14 @@ const IpGeolocation = async (ip = "8.8.8.8") => {
   lat = data.location.lat;
   lng = data.location.lng;
 
-  var map = L.map("map").setView([lat, lng], 1);
-  var marker = L.marker([lat, lng]).addTo(map);
+  if (map === undefined) {
+    map = L.map("map").setView([lat, lng], 1);
+    marker = L.marker([lat, lng]).addTo(map);
+  } else {
+    map.remove();
+    map = L.map("map").setView([lat, lng], 1);
+    marker = L.marker([lat, lng]).addTo(map);
+  }
 
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
